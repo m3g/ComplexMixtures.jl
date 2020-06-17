9a0c44bf3fbcc2898abd7b82111e2c3786de185c
Leandro Martinez <leandro@iqm.unicamp.br>
2020-06-16 21:38:46 -0300

    fixed rotation

--- src/move.jl
+++ src/move.jl
@@ -4,6 +4,8 @@
 # modifyies the vector x
 #
 
+using Rotations
+
 function move!(x :: Array{Float64}, aux :: MoveAux)
   
   n = size(x,1)
@@ -21,9 +23,9 @@ function move!(x :: Array{Float64}, aux :: MoveAux)
     y[i,1] = x[i,1] - oldcm[1]
     y[i,2] = x[i,2] - oldcm[2]
     y[i,3] = x[i,3] - oldcm[3]
-    x[i,1] = newcm[1] + x[i,1]*A[1,1] + x[i,2]*A[2,1] + x[i,3]*A[3,1]    
-    x[i,2] = newcm[2] + x[i,1]*A[1,2] + x[i,2]*A[2,2] + x[i,3]*A[3,2]    
-    x[i,3] = newcm[3] + x[i,1]*A[1,3] + x[i,2]*A[2,3] + x[i,3]*A[3,3]    
+    x[i,1] = newcm[1] + y[i,1]*A[1,1] + y[i,2]*A[2,1] + y[i,3]*A[3,1]    
+    x[i,2] = newcm[2] + y[i,1]*A[1,2] + y[i,2]*A[2,2] + y[i,3]*A[3,2]    
+    x[i,3] = newcm[3] + y[i,1]*A[1,3] + y[i,2]*A[2,3] + y[i,3]*A[3,3]    
   end
 
 end
