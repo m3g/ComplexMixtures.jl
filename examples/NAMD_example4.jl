624ad9ec306e8f576fe6e2b5b26598685f637b78
Leandro Martinez <leandro@iqm.unicamp.br>
2020-07-05 22:50:56 -0300

    updated some examples

--- examples/NAMD_example4.jl
+++ examples/NAMD_example4.jl
@@ -36,7 +36,6 @@ options = MDDF.Options(output="example.dat",binstep=0.2)
 
 # Run MDDF calculation, and get the resutls in the R structure
 R = MDDF.mddf_linkedcells(trajectory,options)
-#R = MDDF.mddf_naive(trajectory,options)
 
 include("./plots.jl")
 
--- examples/NAMD_example5.jl
+++ examples/NAMD_example5.jl
@@ -13,18 +13,14 @@ solute = MDDF.Solute( solute_indexes, nmols=1 )
 # The solvent is TMAO, which has 14 atoms. Use the natomspermol to indicate how many
 # atoms each molecule has, such that there is no ambiguity on how to split the coordinates 
 # of the selection into individual molecules.
-#solvent_indexes = [ atom.index for atom in filter( atom -> atom.resname == "TMAO", atoms ) ]
-#solvent = MDDF.Solvent( solvent_indexes, natomspermol=14 )
-
-solvent_indexes = [ atom.index for atom in filter( atom -> (atom.resname == "TIP3" && atom.resnum <= 1000 ), atoms ) ]
-#solvent_indexes = [ atom.index for atom in filter( atom -> atom.resname == "TIP3", atoms ) ]
+solvent_indexes = [ atom.index for atom in filter( atom -> atom.resname == "TIP3", atoms ) ]
 solvent = MDDF.Solvent( solvent_indexes, natomspermol=3 )
 
 # Initialize trajectroy data structure and open input stream
 trajectory = MDDF.NamdDCD("./trajectory.dcd",solute,solvent)
 
 # Input options for the calcualtion
-options = MDDF.Options(output="example.dat",binstep=0.2,lastframe=1)
+options = MDDF.Options(output="example.dat",binstep=0.2)
 
 # Run MDDF calculation, and get the resutls in the R structure
 R = MDDF.mddf_linkedcells(trajectory,options)
