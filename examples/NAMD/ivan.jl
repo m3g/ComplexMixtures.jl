484388e60406aac3fd40e681e7dc797ba32d81c1
Leandro Martinez <leandro@iqm.unicamp.br>
2020-08-04 15:13:01 -0300

    added ivan example

--- examples/NAMD/ivan.jl
+++ examples/NAMD/ivan.jl
@@ -6,21 +6,24 @@ using MDDF
 using PDBTools
 using Plots
 
-dir=/home/leandro/Drive/Alunos/Ivan/PCCP_revision
+dir="/home/leandro/Drive/Alunos/Ivan/PCCP_revision"
 
 # Here we use the PDBTools package to read the pdb file (from http://github.com/m3g/PDBTools)
 atoms = PDBTools.readPDB("$dir/6Mnative.pdb")
 
 # Set solute and solvent
-solute = MDDF.Selection(atoms,"protein",nmols=1 )
-solvent = MDDF.Selection(atoms,"water",natomspermol=3)
+
+protein = PDBTools.select(atoms,"protein")
+solute = MDDF.Selection(protein,nmols=1 )
+
+water = PDBTools.select(atoms,"water")
+solvent = MDDF.Selection(water,natomspermol=3)
 
 # Initialize trajectroy data structure and open input stream
 trajectory = MDDF.Trajectory("$dir/6Mnative.dcd",solute,solvent)
 
 R = MDDF.mddf(trajectory)
 MDDF.save(R,"ivan.json")
-
 
 plot(R.d,R.mddf,linewidth=2,label="")
 savefig("./plots.pdf")
---
+++ src/contrib.jl
@@ -0,0 +1,32 @@
+#
+# Extract the contribution of a given atom type selection from teh 
+# solute or solvent atomic contributions to the MDDF 
+#
+# s here is the solute or solvent selection
+# atom_contributions is the R.solute_atom or R.solvent_atom arrays of the Result structure
+
+# If a list of atom names is provided: 
+function contrib(s :: Selection, atom_contributions :: Array{Float64}, namelist :: Vector{String})
+  nbins = size(atom_contributions,1)
+  natoms = size(atom_contributions,2)
+  c = zeros(nbins)
+  for i in 1:natoms
+    if s.name[i] in namelist
+      c += atom_contributions[:,i]
+    end
+  end
+  return c
+end
+
+# If a list of indexes (within the a molecule) is provided
+function contrib(s :: Selection, atom_contributions :: Array{Float64}, indexlist :: Vector{Int64})
+  nbins = size(atom_contributions,1)
+  natoms = size(atom_contributions,2)
+  c = zeros(nbins)
+  for i in 1:natoms
+    if itype(i,s.natomspermol) in indexlist
+      c += atom_contributions[:,i]
+    end
+  end
+  return c
+end
