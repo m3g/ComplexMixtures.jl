99064e26f4a512bf229d4e0312d2b1e4d2f1ab70
Leandro Martinez <leandro@iqm.unicamp.br>
2020-08-06 18:45:02 -0300

    fixed whichtypes

--- src/which_types.jl
+++ src/which_types.jl
@@ -1,10 +1,12 @@
+#
 # Function that returns the list of the types of atoms in a selection
 # (that is, the indexes of the atom in the sequence of atoms of a molecule
 # even if the indexes contain the indexes of many molecules of the
 # same type
-
+#
 function which_types(s :: Selection, indexes :: Vector{Int64})
   selected_types = Vector{Int64}(undef,0)
+  ntypes = 0
   for i in indexes
     isel = findfirst( ind -> ind == i, s.index )
     if isel == nothing
@@ -12,7 +14,14 @@ function which_types(s :: Selection, indexes :: Vector{Int64})
       return 0
     else
       it = itype(isel,s.natomspermol)  
-      push!(selected_types,it)
+      if ! (it in selected_types)
+        push!(selected_types,it)
+        ntypes += 1
+        if ntypes == s.natomspermol 
+          println(" Warning: All possible types of atoms of this selection were selected.")
+          return selected_types
+        end
+      end
     end
   end
   return selected_types
