8f612484b18a91aa4bde4f19b6b6a70db01e9d67
Leandro Martinez <leandro@iqm.unicamp.br>
2020-08-07 11:32:55 -0300

    adjusted conbrib function

--- src/contrib.jl
+++ src/contrib.jl
@@ -9,27 +9,40 @@
 # If a list of indexes of atoms of the simulation is provided 
 #
 
-function contrib(s :: Selection, atom_contributions :: Array{Float64}, indexes :: Vector{Int64})
-  # Sum the contributions of the selected types to the contributions to be summed up    
-  selected_types = which_types(s, indexes)
+function contrib(s :: Selection, atom_contributions :: Array{Float64}, selected_types :: Vector{Int64})
   nbins = size(atom_contributions,1)
   c = zeros(nbins)
   for it in selected_types
+    if it > s.natomspermol
+      error("The index list contains atoms with indexes greater than the number of atoms of the molecule.")
+    end
     c += atom_contributions[:,it]
   end
   return c
 end
 
 #
-# Use this with, for example:
+# If a list of atom names is provided
 #
-# atoms = PDBTools.readPDB("file.pdb")
-# args = PDBTools.selindex(atoms,"resname ARG")
-#
+function contrib(s :: Selection, atom_contributions :: Array{Float64}, names :: Vector{String})
+  selected_types = Vector{Int64}(undef,0)
+  for name in names
+    index = findfirst( x -> x == name, s.names )
+    if index == nothing
+      error(" Atom in input list is not part of solvent (or solute).")
+    end
+    push!(selected_types,index)
+  end
+  return contrib(s, atom_contributions, selected_types)
+end
 
+#
 # If a list of atoms of PDBTools.Atom is provided
+#
 function contrib(s :: Selection, atom_contributions :: Array{Float64}, atoms :: Vector{PDBTools.Atom})
   indexes = [ atom.index for atom in atoms ]
-  return contrib(s, atom_contributions, indexes)
+  # Sum the contributions of the selected types to the contributions to be summed up    
+  selected_types = which_types(s, indexes)
+  return contrib(s, atom_contributions, selected_types)
 end
 
