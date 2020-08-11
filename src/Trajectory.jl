9dfaf4ec2d4d65ede99a0e2e6304c496d6936766
Leandro Martinez <leandro@iqm.unicamp.br>
2020-08-11 14:48:17 -0300

    changed default trajectory to NamdDCD for dcd files

--- src/Trajectory.jl
+++ src/Trajectory.jl
@@ -1,15 +1,28 @@
-# Default reading with the Chemfiles infrastructure
+#
+# Default reading with the Chemfiles infrastructure, except for DCD files,
+# because of the memory issue (https://github.com/chemfiles/Chemfiles.jl/issues/44)
+#
 
-Trajectory( filename :: String, 
-            solute :: Selection, solvent :: Selection;
-            format :: String = "") =
-  ChemFile(filename,solute,solvent,format=format)
+function Trajectory( filename :: String, 
+                     solute :: Selection, solvent :: Selection;
+                     format :: String = "")
+  if format == "dcd" || FileOperations.file_extension(filename) == "dcd"
+    NamdDCD(filename,solute,solvent)
+  else
+    ChemFile(filename,solute,solvent,format=format)
+  end
+end
 
 # If only one selection is provided, assume that the solute and the 
 # solvent are the same
 
-Trajectory( filename :: String, 
-            solvent :: Selection; 
-            format :: String = "") =
-  ChemFile(filename,solvent,solvent,format=format)
+function Trajectory( filename :: String, 
+                    solvent :: Selection; 
+                    format :: String = "")
+  if format == "dcd" || FileOperations.file_extension(filename) == "dcd"
+    NamdDCD(filename,solvent,solvent)
+  else
+    ChemFile(filename,solvent,solvent,format=format)
+  end
+end
 
