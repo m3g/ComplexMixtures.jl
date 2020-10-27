d2bace6f39ca6999f3101c9594c6e78ef75fa082
Leandro Martinez <leandro@iqm.unicamp.br>
2020-10-27 16:06:31 -0300

    added merge test

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,6 +24,11 @@ const CM = ComplexMixtures
   t = isapprox(R,R_save,debug=true) 
   @test t
   
+  # Test save and load
+  CM.save(R,"test.json")
+  R_load = CM.load("test.json")
+  @test R_load ≈ R_save
+
   # Example 2: water-tmao
   
   # CM.save(R,"$dir/water_tmao.json")
@@ -134,6 +139,30 @@ end
   @test t
   
 end 
+
+@testset "Merge" begin
+
+  dir="./data/NAMD"
+  atoms = readPDB("$dir/structure.pdb")  
+  tmao = CM.Selection(select(atoms,"resname TMAO"),natomspermol=14)
+  water = CM.Selection(select(atoms,"water"),natomspermol=3)
+  
+  # CM.save(R,"$dir/merged.json")
+  R_save = CM.load("$dir/merged.json")
+
+  options = CM.Options(firstframe=1,lastframe=2,seed=1234567,nthreads=1,silent=true)
+  traj = CM.Trajectory("$dir/trajectory.dcd",tmao,water) 
+  R1 = CM.mddf(traj,options)
+
+  options = CM.Options(firstframe=3,lastframe=6,seed=1234567,nthreads=1,silent=true)
+  traj = CM.Trajectory("$dir/trajectory.dcd",tmao,water) 
+  R2 = CM.mddf(traj,options)
+
+  R = CM.merge([R1,R2])
+  t = isapprox(R,R_save,debug=true) 
+  @test t
+
+end
 
 
 
