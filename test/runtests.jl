fca92b8d946476a4b6b7e31c0c1705dbf1bac74e
Leandro Martinez <leandro@iqm.unicamp.br>
2020-11-01 13:28:51 -0300

    fixed runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,47 +3,47 @@ using ComplexMixtures, PDBTools
 using Random
 const CM = ComplexMixtures
 
-@testset "Internals" begin
+@testset "Allocations" begin
 
   dir="./data/NAMD"
   atoms = readPDB("$dir/structure.pdb")  
 
   options = CM.Options(lastframe=1,seed=1234567,nthreads=1,silent=true)
   t_options = @allocated options = CM.Options(lastframe=1,seed=1234567,nthreads=1,silent=true)
-  @test t_options.bytes == 0
+  @test t_options == 0
 
   protein = CM.Selection(select(atoms,"protein"),nmols=1)
   t_selection1 = @allocated protein = CM.Selection(select(atoms,"protein"),nmols=1)
-  @test t_selection1.bytes == 3064544
+  @test t_selection1 == 3064544
 
   tmao = CM.Selection(select(atoms,"resname TMAO"),natomspermol=14)
   t_selection2 = @allocated tmao = CM.Selection(select(atoms,"resname TMAO"),natomspermol=14)
-  @test t_selection2.bytes == 7080880
+  @test t_selection2 == 7080880
 
   traj = CM.Trajectory("$dir/trajectory.dcd",protein,tmao) 
   t_trajectory = @allocated traj = CM.Trajectory("$dir/trajectory.dcd",protein,tmao) 
-  @test t_trajectory.bytes == 660240
+  @test t_trajectory == 660240
 
   samples = CM.Samples(md=(traj.solvent.nmols-1)/2,random=options.n_random_samples)
   t_samples = @allocated samples = CM.Samples(md=(traj.solvent.nmols-1)/2,random=options.n_random_samples)
-  @test t_samples.bytes == 256
+  @test t_samples == 256
 
   R = CM.Result(traj,options)
   t_result = @allocated R = CM.Result(traj,options)
-  @test t_result.bytes == 5968352
+  @test t_result == 5968352
 
   framedata = CM.FrameData(traj,R)
   t_framedata = @allocated framedata = CM.FrameData(traj,R)
-  @test t_framedata.bytes == 229984
+  @test t_framedata == 229984
 
   CM.nextframe!(traj)
   t_nextframe = @allocated CM.nextframe!(traj)
-  @test t_nextframe.bytes == 624
+  @test t_nextframe == 624
 
   iframe = 1
   CM.mddf_frame!(iframe,framedata,options,R)
   t_mddf_frame = @allocated CM.mddf_frame!(iframe,framedata,options,R)
-  @test t_mddf_frame.bytes == 144
+  @test t_mddf_frame == 144
 
 end
 
