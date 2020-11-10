aad8795f410678a0e4fbaf53fdea7e08ff6efd21
Leandro Martinez <leandro@iqm.unicamp.br>
2020-11-10 12:50:23 -0300

    runs allocation test only on linux-1.5.2

--- test/allocations.jl
+++ test/allocations.jl
@@ -4,48 +4,53 @@ const CM = ComplexMixtures
 
 @testset "Allocations" begin
 
-  dir="./data/NAMD"
-  atoms = readPDB("$dir/structure.pdb")  
+  if Sys.islinux() && VERSION == v"1.5.2" 
 
-  options = CM.Options(lastframe=1,nthreads=1,silent=true,seed=321,StableRNG=true)
-  t_options = @allocated CM.Options(lastframe=1,seed=321,StableRNG=true,nthreads=1,silent=true)
-  @test t_options == 0
+    dir="./data/NAMD"
+    atoms = readPDB("$dir/structure.pdb")  
 
-  protein = CM.Selection(select(atoms,"protein"),nmols=1)
-  t_selection1 = @allocated CM.Selection(select(atoms,"protein"),nmols=1)
-  @test t_selection1 == 3064544
+    options = CM.Options(lastframe=1,nthreads=1,silent=true,seed=321,StableRNG=true)
+    t_options = @allocated CM.Options(lastframe=1,seed=321,StableRNG=true,nthreads=1,silent=true)
+    @test t_options == 0
 
-  tmao = CM.Selection(select(atoms,"resname TMAO"),natomspermol=14)
-  t_selection2 = @allocated CM.Selection(select(atoms,"resname TMAO"),natomspermol=14)
-  @test t_selection2 == 7080880
+    protein = CM.Selection(select(atoms,"protein"),nmols=1)
+    t_selection1 = @allocated CM.Selection(select(atoms,"protein"),nmols=1)
+    @test t_selection1 == 3064544
 
-  traj = CM.Trajectory("$dir/trajectory.dcd",protein,tmao) 
-  t_trajectory = @allocated CM.Trajectory("$dir/trajectory.dcd",protein,tmao) 
-  @test abs(t_trajectory - 660240) < 124
+    tmao = CM.Selection(select(atoms,"resname TMAO"),natomspermol=14)
+    t_selection2 = @allocated CM.Selection(select(atoms,"resname TMAO"),natomspermol=14)
+    @test t_selection2 == 7080880
 
-  samples = CM.Samples(md=(traj.solvent.nmols-1)/2,random=options.n_random_samples)
-  t_samples = @allocated CM.Samples(md=(traj.solvent.nmols-1)/2,random=options.n_random_samples)
-  @test t_samples == 256
+    traj = CM.Trajectory("$dir/trajectory.dcd",protein,tmao) 
+    t_trajectory = @allocated CM.Trajectory("$dir/trajectory.dcd",protein,tmao) 
+    @test abs(t_trajectory - 660240) < 124
 
-  R = CM.Result(traj,options)
-  t_result = @allocated CM.Result(traj,options)
-  @test t_result == 5968736
+    samples = CM.Samples(md=(traj.solvent.nmols-1)/2,random=options.n_random_samples)
+    t_samples = @allocated CM.Samples(md=(traj.solvent.nmols-1)/2,random=options.n_random_samples)
+    @test t_samples == 256
 
-  framedata = CM.FrameData(traj,R)
-  t_framedata = @allocated CM.FrameData(traj,R)
-  @test t_framedata == 228528
+    R = CM.Result(traj,options)
+    t_result = @allocated CM.Result(traj,options)
+    @test t_result == 5968736
 
-  CM.nextframe!(traj)
-  t_nextframe = @allocated CM.nextframe!(traj)
-  @test t_nextframe == 624
+    framedata = CM.FrameData(traj,R)
+    t_framedata = @allocated CM.FrameData(traj,R)
+    @test t_framedata == 228528
 
-  RNG = CM.init_random(options)
-  t_RNG = @allocated CM.init_random(options) 
-  @test t_RNG == 0
+    CM.nextframe!(traj)
+    t_nextframe = @allocated CM.nextframe!(traj)
+    @test t_nextframe == 624
 
-  CM.mddf_frame!(1,framedata,options,RNG,R)
-  t_mddf_frame = @allocated CM.mddf_frame!(1,framedata,options,RNG,R)
-  @test t_mddf_frame == 160
+    RNG = CM.init_random(options)
+    t_RNG = @allocated CM.init_random(options) 
+    @test t_RNG == 0
+
+    CM.mddf_frame!(1,framedata,options,RNG,R)
+    t_mddf_frame = @allocated CM.mddf_frame!(1,framedata,options,RNG,R)
+    @test t_mddf_frame == 160
+
+  end
+  @test true
 
 end
 
