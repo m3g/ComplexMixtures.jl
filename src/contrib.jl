29e6cbeb73ebb163d9031e86bbe5314e87ccdc0d
Leandro Martinez <leandro@iqm.unicamp.br>
2021-04-23 11:20:21 -0300

    implementing compatibility with PDBTools.Residue

--- src/contrib.jl
+++ src/contrib.jl
@@ -1,5 +1,5 @@
 #
-# Extract the contribution of a given atom type selection from teh 
+# Extract the contribution of a given atom type selection from the 
 # solute or solvent atomic contributions to the MDDF 
 #
 # s here is the solute or solvent selection
@@ -8,11 +8,10 @@
 #
 # If a list of indexes of atoms of the simulation is provided 
 #
-
-function contrib(s :: Selection, atom_contributions :: Array{Float64}, selected_types :: Vector{Int64})
+function contrib(s::Selection, atom_contributions::Array{Float64}, indexes::Vector{Int64})
   nbins = size(atom_contributions,1)
   c = zeros(nbins)
-  for it in selected_types
+  for it in indexes
     if it > s.natomspermol
       error("The index list contains atoms with indexes greater than the number of atoms of the molecule.")
     end
@@ -24,25 +23,43 @@ end
 #
 # If a list of atom names is provided
 #
-function contrib(s :: Selection, atom_contributions :: Array{Float64}, names :: Vector{String})
-  selected_types = Vector{Int64}(undef,0)
+function contrib(s::Selection, atom_contributions::Array{Float64}, names::Vector{String})
+  indexes = Vector{Int64}(undef,0)
   for name in names
     index = findfirst( x -> x == name, s.names )
     if index == nothing
       error(" Atom in input list is not part of solvent (or solute).")
     end
-    push!(selected_types,index)
+    push!(indexes,index)
   end
-  return contrib(s, atom_contributions, selected_types)
+  return contrib(s, atom_contributions, indexes)
 end
 
 #
 # If a list of atoms of PDBTools.Atom is provided
 #
-function contrib(s :: Selection, atom_contributions :: Array{Float64}, atoms :: Vector{PDBTools.Atom})
+function contrib(s::Selection, atom_contributions::Array{Float64}, atoms::Vector{PDBTools.Atom}; warning=true)
+  (warning && s.nmols > 1) && warning_nmols_types() 
   indexes = [ atom.index for atom in atoms ]
-  # Sum the contributions of the selected types to the contributions to be summed up    
+  # Check which types of atoms belong to this selection
   selected_types = which_types(s, indexes)
   return contrib(s, atom_contributions, selected_types)
 end
+
+#
+# If a residue of type PDBTools.Residue is provided
+#
+function contrib(s::Selection, atom_contributions::Array{Float64}, residue::Residue)
+  (warning && s.nmols > 1) && warning_nmols_types() 
+  indexes = collect(residue.range)
+  # Check which types of atoms belong to this selection
+  selected_types = which_types(s, indexes)
+  return contrib(s, atom_contributions, selected_types)
+end
+
+function warning_nmols_types()
+  println("WARNING: there is more than one molecule in this solute. "*
+          "Contributions are summed over all atoms of the same type.")
+end
+
 
--- src/which_types.jl
+++ src/which_types.jl
@@ -1,10 +1,24 @@
-#
-# Function that returns the list of the types of atoms in a selection
-# (that is, the indexes of the atom in the sequence of atoms of a molecule
-# even if the indexes contain the indexes of many molecules of the
-# same type
-#
-function which_types(s :: Selection, indexes :: Vector{Int})
+"""
+
+`which_types(s::Selection, indexes::Vector{Int})`
+
+Function that returns the list of the indexes of the types of the atoms in a
+selection. For example, if a selection corresponds to a solvent of water molecules:
+There are three types, 1, 2, and 3, corresponding to the three atoms of the
+water molecule. If the indexes provided are, for instance, 11, 12, and 13, 
+corresponding to a water molecule, this function will return 1, 2 and 3.
+
+This is used to get equivalent-atom contributions to the distribution functions.
+For example, the input indexes span all water molecules, the output of this
+function will be still the three indexes corresponding to the three types
+of atoms that exist in a water molecule. 
+
+It is not possible to compute the contribution of *one* individual water molecule
+if the distribution function was computed for all molecules. Thus, the necessity
+to identify the types of atoms involved in a selection.   
+
+"""
+function which_types(s::Selection, indexes::Vector{Int})
   selected_types = Vector{Int}(undef,0)
   ntypes = 0
   for i in indexes
