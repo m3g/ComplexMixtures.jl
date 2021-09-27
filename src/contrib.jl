3fc68de12fba30177302e7c9c9e9f0cedee5d3e4
Leandro Martinez <leandro@iqm.unicamp.br>
2021-09-27 17:41:13 -0300

    fixed check of index

--- src/contrib.jl
+++ src/contrib.jl
@@ -32,7 +32,7 @@ function contrib(s::Selection, atom_contributions::Array{Float64}, names::Vector
   indexes = Vector{Int}(undef,0)
   for name in names
     index = findall(isequal(name), s.names)
-    if length(index) > 0
+    if length(index) == 0
       error(" Atom in input list is not part of solvent (or solute).")
     end
     append!(indexes,index)
