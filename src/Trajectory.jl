b493f8739a9a1123008f35a753b7581b1a09918f
Leandro Martinez <lmartine@unicamp.br>
2022-10-31 10:08:38 -0300

    formatting changes

--- src/Trajectory.jl
+++ src/Trajectory.jl
@@ -1,45 +1,27 @@
 """
+    Trajectory(filename::String, solute::Selection, solvent::Selection; format::String = "", chemfiles = false)
 
-```
-Trajectory
-```
+Trajectory constructor data type. 
 
-Trajectory data type. 
-Default reading with the Chemfiles infrastructure, except for DCD and PDB trajectory
+Defaults to reading with the Chemfiles infrastructure, except for DCD and PDB trajectory
 files, if the "PDBTraj" option is provided.
+
 See memory issue (https://github.com/chemfiles/Chemfiles.jl/issues/44)
 
 """
 abstract type Trajectory end
 
-function Trajectory(
-    filename::String,
-    solute::Selection,
-    solvent::Selection;
-    format::String = "",
-    chemfiles = false,
-)
-    if !chemfiles
-        if format == "dcd" || FileOperations.file_extension(filename) == "dcd"
-            NamdDCD(filename, solute, solvent)
-        elseif format == "PDBTraj"
-            PDBTraj(filename, solute, solvent)
-        else
-            ChemFile(filename, solute, solvent, format = format)
-        end
+function Trajectory(filename::String, solute::Selection, solvent::Selection; format::String = "", chemfiles = false)
+    if !chemfiles && (format == "dcd" || FileOperations.file_extension(filename) == "dcd")
+        trajectory = NamdDCD(filename, solute, solvent)
+    elseif !chemfiles && format == "PDBTraj"
+        trajectory = PDBTraj(filename, solute, solvent)
     else
-        ChemFile(filename, solute, solvent, format = format)
+        trajectory = ChemFile(filename, solute, solvent, format = format)
     end
+    return trajectory
 end
 
-# If only one selection is provided, assume that the solute and the 
-# solvent are the same
-
-function Trajectory(
-    filename::String,
-    solvent::Selection;
-    format::String = "",
-    chemfiles = false,
-)
+# If only one selection is provided, assume that the solute and the solvent are the same
+Trajectory(filename::String, solvent::Selection; format::String = "", chemfiles = false) =
     Trajectory(filename, solvent, solvent, format = format, chemfiles = chemfiles)
-end
