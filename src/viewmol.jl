1908678a433ad98b8c5dff9f8d4ca87e3451b3e5
Leandro Martinez <lmartine@unicamp.br>
2022-11-10 15:37:28 -0300

    split into mol_range

--- src/viewmol.jl
+++ src/viewmol.jl
@@ -1,14 +1,27 @@
 """
+    mol_range(i, n)
+
+$(INTERNAL)
+
+Given the index `i` and the number of atoms per molecule `n`, returns the range of indices 
+of of an array of coordinates that corresponds to the molecule `i`.
+
+"""
+function mol_range(i, n)
+    first = (i - 1) * n + 1
+    last = first + n - 1
+    return first:last
+end
+
+"""
     viewmol(i::Int, x::Vector{T}, n::Int) where T
+
+$(INTERNAL)
 
 Returns a view of a coordinate vector corresponding to the atoms of a molecule with index i. n is the number of atoms of the molecule.
 
 """
-function viewmol(i::Int, x::Vector{T}, n::Int) where {T}
-    first = (i - 1) * n + 1
-    last = first + n - 1
-    return @view(x[first:last])
-end
+viewmol(i::Int, x::Vector{T}, n::Int) where {T} = @view(x[mol_range(i,n)])
 
 # From the structure of the solute or solvent
 viewmol(i::Int, x::Vector{T}, s::Selection) where {T} = viewmol(i, x, s.natomspermol)
