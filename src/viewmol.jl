8d6f1dd9a59d5678562a244994edf2f0198c09ff
Leandro Martinez <leandro@iqm.unicamp.br>
2022-11-11 16:07:33 -0300

    support solsummary

--- src/viewmol.jl
+++ src/viewmol.jl
@@ -23,5 +23,6 @@ Returns a view of a coordinate vector corresponding to the atoms of a molecule w
 """
 viewmol(i::Int, x::Vector{T}, n::Int) where {T} = @view(x[mol_range(i,n)])
 
-# From the structure of the solute or solvent
-viewmol(i::Int, x::Vector{T}, s::Selection) where {T} = viewmol(i, x, s.natomspermol)
+# From the selection of the solute or solvent
+viewmol(i::Int, x::Vector{T}, s::Union{Selection,SolSummary}) where {T} = viewmol(i, x, s.natomspermol)
+
