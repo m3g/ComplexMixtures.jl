21e604092a370700c6aa74149fb09d1e27cb2769
Leandro Martinez <lmartine@unicamp.br>
2022-11-23 14:40:58 -0300

    update test file

--- test/v0.5.10/test.jl
+++ test/v0.5.10/test.jl
@@ -10,6 +10,9 @@ options = Options(stride=5,seed=321,StableRNG=true,nthreads=1,silent=true)
 protein = Selection(select(atoms, "protein"), nmols=1)
 tmao = Selection(select(atoms, "resname TMAO"), natomspermol=14)
 traj = Trajectory("$(data_dir)/trajectory.dcd", protein, tmao)
-R = mddf(traj, options)
+R1 = mddf(traj, options)
 
+water = Selection(select(atoms, "water"), natomspermol=3)
+traj = Trajectory("$(data_dir)/trajectory.dcd", protein, water)
+R2 = mddf(traj, options)
 
