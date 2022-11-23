f040172720da54bc9add9a480b4b65243fc7e8d1
Leandro Martinez <lmartine@unicamp.br>
2022-11-23 14:38:24 -0300

    split counters for true and ideal distributions

--- src/updatecounters.jl
+++ src/updatecounters.jl
@@ -24,28 +24,29 @@ $(INTERNAL)
 Function that updates the minimum-distance counters in `R`
 
 """
-function updatecounters!(R::Result, system::AbstractPeriodicSystem; random::Bool=false)
-    if !random
-        for md in system.list
-            !md.within_cutoff && continue
-            ibin = setbin(md.d, R.options.binstep)
-            R.md_count[ibin] += 1
-            R.solute_atom[ibin, itype(md.i, R.solute)] += 1
-            R.solvent_atom[ibin, itype(md.j, R.solvent)] += 1
-            if md.ref_atom_within_cutoff
-                ibin = setbin(md.d_ref_atom, R.options.binstep)
-                R.rdf_count[ibin] += 1
-            end
+function updatecounters!(R::Result, system::AbstractPeriodicSystem)
+    for md in system.list
+        !md.within_cutoff && continue
+        ibin = setbin(md.d, R.options.binstep)
+        R.md_count[ibin] += 1
+        R.solute_atom[ibin, itype(md.i, R.solute)] += 1
+        R.solvent_atom[ibin, itype(md.j, R.solvent)] += 1
+        if md.ref_atom_within_cutoff
+            ibin = setbin(md.d_ref_atom, R.options.binstep)
+            R.rdf_count[ibin] += 1
         end
-    else
-        for md in system.list
-            !md.within_cutoff && continue
-            ibin = setbin(md.d, R.options.binstep)
-            R.md_count_random[ibin] += 1
-            if md.ref_atom_within_cutoff
-                ibin = setbin(md.d_ref_atom, R.options.binstep)
-                R.rdf_count_random[ibin] += 1
-            end
+    end
+    return R
+end
+# Update counters for the ideal gas distributions
+function updatecounters!(R::Result, system::AbstractPeriodicSystem, ::Val{:random})
+    for md in system.list
+        !md.within_cutoff && continue
+        ibin = setbin(md.d, R.options.binstep)
+        R.md_count_random[ibin] += 1
+        if md.ref_atom_within_cutoff
+            ibin = setbin(md.d_ref_atom, R.options.binstep)
+            R.rdf_count_random[ibin] += 1
         end
     end
     return R
