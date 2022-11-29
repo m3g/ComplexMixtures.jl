c0f1454d238aa4aeb98fc720fc224a0d7e3fde60
Leandro Martinez <lmartine@unicamp.br>
2022-11-29 16:06:51 -0300

    update runtests to run TestItems

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,2 @@
-using Test
-using StableRNGs
-using ComplexMixtures, PDBTools
-using Random
-const CM = ComplexMixtures
-
-include("./namd.jl")
-include("./namd_chemfiles.jl")
-include("./gromacs.jl")
-include("./pdb.jl")
-include("./merge.jl")
-
+using TestItemRunner
+@run_package_tests
