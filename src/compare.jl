5881f22b39a18a98c628b5d50b29208d5c668c29
Leandro Martinez <lmartine@unicamp.br>
2023-12-01 15:22:21 -0300

    set single comparision methods for all types

---
+++ src/compare.jl
@@ -0,0 +1,46 @@
+"""
+
+$(INTERNAL)
+
+Union of types to define comparison operators.
+
+"""
+const ComplexMixturesTypes = Union{Result,Density,Volume,SolSummary,Options}
+
+"""
+    Base.isapprox(r1::T, r2::T; debug=false) where T <: CMTypes
+
+$(INTERNAL)
+
+Function to test if two runs offered similar results. Mostly used in the package testing routines.
+
+"""
+Base.isapprox(x::T, y::T; debug = false) where {T<:ComplexMixturesTypes} =
+    _compare(isapprox, x, y; debug = debug)
+
+import Base: ==
+==(x::T, y::T; debug = false) where {T<:ComplexMixturesTypes} = _compare(==, x, y; debug = debug)
+
+function _compare(_similar::F, x::T, y::T; debug = false) where {T<:ComplexMixturesTypes} where {F<:Function}
+    check = true
+    diff_list = Symbol[]
+    for field in fieldnames(T)
+        if field in [ :files, :Version ]
+            continue
+        end
+        xf = getfield(x, field)
+        yf = getfield(y, field)
+        if !(_similar(xf, yf))
+            check = false
+            if debug
+                push!(diff_list, field)
+            end
+        end
+    end
+    if debug
+        for field in diff_list
+            println(" Data in $field field differ. ")
+        end
+    end
+    return check
+end
