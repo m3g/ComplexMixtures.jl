1ec28d53d8ea46c5d70f2e55d459f5b870fb9525
Leandro Martinez <lmartine@unicamp.br>
2023-12-20 11:42:12 -0300

    commment internal docs

--- src/viewmol.jl
+++ src/viewmol.jl
@@ -1,26 +1,24 @@
-"""
+#=
     mol_range(imol, n_atoms_per_molecule)
-
-$(INTERNAL)
 
 Given the index and the number of atoms per molecule, returns the range of indices 
 of of an array of coordinates that corresponds to the molecule.
 
-"""
+=#
 function mol_range(imol, n_atoms_per_molecule)
     first = (imol - 1) * n_atoms_per_molecule + 1
     last = first + n_atoms_per_molecule - 1
     return first:last
 end
 
-"""
+#=
     viewmol(i::Int, x::Vector{T}, n::Int) where T
 
 $(INTERNAL)
 
 Returns a view of a coordinate vector corresponding to the atoms of a molecule with index i. n is the number of atoms of the molecule.
 
-"""
+=#
 viewmol(i::Int, x::Vector{T}, n::Int) where {T} = @view(x[mol_range(i, n)])
 
 # From the selection of the solute or solvent
