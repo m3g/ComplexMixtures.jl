e1a68ab1ca2889fdcff2fac86df689f4ddf4f359
Leandro Martinez <lmartine@unicamp.br>
2024-01-22 11:40:49 -0300

    implementing contributions with new structres

--- src/tools/contributions.jl
+++ src/tools/contributions.jl
@@ -1,93 +1,76 @@
-"""
-    contributions(s::AtomSelection, atom_contributions::Matrix{Float64}, selection)
 
-Extract the contribution of a given atom type selection from the solute or solvent atomic contributions to the MDDF.
+function _error_contrib_index(i, itype)
+    throw(ArgumentError(chomp("""
+        Atom index $i with type index $itype are not available in the contribution matrix. 
+        There is something wrong in the way the group selection was provided.
+    """)))
+end
 
-`s` here is the solute or solvent selection (type `ComplexMixtures.AtomSelection`)
-`atom_contributions` is the `R.solute_atom` or `R.solvent_atom` arrays of the `Result` structure,
-and the last argument is the selection of atoms from the solute to be considered, given as a list 
-of indices, list of atom names, vector of `PDBTools.Atom`s, or a `PDBTools.Residue`. 
+function _error_contrib_name(name)
+    throw(ArgumentError(chomp("""
+        Atom name $name is not available in the contribution matrix. 
+        There is something wrong in the way the group selection was provided.
+    """)))
+end
 
-## Extended help
-
-For selections of one molecule, the function has an additional keyword option `first_atom_is_ref` that is `false` by default. 
-If set to `true`, the index first atom of the selection is considered as a reference atom. For example if a solute has 100 atoms,
-but its first atom in the PDB file is number 901, the selection of indices `[1, 2, 3]` will refer to atoms
-with indices `[901, 902, 903]`.
-
-"""
-function contributions(
-    s::AtomSelection,
-    atom_contributions::Matrix{Float64},
-    indices::Vector{Int};
-    first_atom_is_ref = false,
-)
-    nbins = size(atom_contributions, 1)
-    c = zeros(nbins)
-    # If the selection is a single molecule, the indices can be anything (as they are the numbers printed
-    # in the PDB file)
-    if s.nmols == 1
-        # If the first atom is a reference atom, the indices are shifted by the index of the first atom
-        if first_atom_is_ref
-            first_index = first(s.index) - 1
+function contributions(R::Result, group::Union{SoluteGroup,SolventGroup})
+    if typeof(group) == SoluteGroup
+        struct_data = R.solute
+        contrib_data = R.solute_group_contributions
+    elseif typeof(group) == SolventGroup
+        struct_data = R.solvent
+        contrib_data = R.solvent_group_contributions
+    end
+    # If the index or group name was provided, just return the corresponding contribution
+    if isnothing(group.indices)
+        igroup = if !isnothing(group.group_index)  
+           group.group_index
+        elseif !isnothing(group.group_name) 
+           findfirst(isequal(group_name), struct_data.group_names)
+        end
+        if isnothing(igroup) || igroup > size(contrib_data, 2)
+            throw(ArgumentError("Group $igroup not found in the contribution matrix."))
+        end
+        return contrib_data[:, igroup]
+    end
+    # If, instead, atom indices or names were provided, sum over the contributions of the atoms.
+    # This sum is different if the structure contanis one or more than one molecule.
+    # If the structure has more than one molecule, than the indices are indices of 
+    # the atoms *within* the molecule. 
+    c = zeros(size(contrib_data, 1))
+    if !isnothing(group.atom_indices) 
+        if struct_data.nmols == 1 
+            for i in group.atom_indices
+                i > size(contrib_data, 2) && _error_contrib_index(i, i)
+                c += @view(contrib_data[:, i])
+            end
         else
-            first_index = 0
-        end
-        for it in indices
-            ind = findfirst(isequal(first_index + it), s.index)
-            if isnothing(ind)
-                error("Index $it of input list not found in selection indices list.")
+            for i in group.atom_indices
+                itype = (i - 1) ÷ struct_data.natomspermol + 1
+                itype > size(contrib_data, 2) && _error_contrib_index(i, itype)
+                c += @view(contrib_data[:, itype])
             end
-            c += @view(atom_contributions[:, ind])
         end
-    # If more than one molecule, the index must correspond to an atom within one molecule
-    else
-        for it in indices
-            if it > s.natomspermol
-                error(
-                    "The index list contains atoms with indices greater than the number of atoms of one molecule.",
-                )
+    end
+    if !isnothing(group.atom_names) 
+        if struct_data.nmols == 1 
+            for name in group.atom_names
+                i = findfirst(isequal(name), struct_data.names)
+                (isnothing(i) || i > size(contrib_data, 2)) && _error_contrib_name(name)
+                c += @view(contrib_data[:, i])
             end
-            c += @view(atom_contributions[:, it])
+        else
+            for name in group.atom_names
+                i = findfirst(isequal(name), struct_data.names)
+                itype = (i - 1) ÷ struct_data.natomspermol + 1
+                (isnothing(i) || itype > size(contrib_data, 2)) && _error_contrib_name(name)
+                c += @view(contrib_data[:, itype])
+            end
         end
     end
     return c
 end
 
-#
-# If a list of atom names is provided
-#
-function contributions(
-    s::AtomSelection,
-    atom_contributions::Matrix{Float64},
-    names::Vector{String},
-)
-    indices = Vector{Int}(undef, 0)
-    for name in names
-        index = findall(isequal(name), s.names)
-        if length(index) == 0
-            error(" Atom in input list is not part of solvent (or solute): $name")
-        end
-        append!(indices, index)
-    end
-    return contributions(s, atom_contributions, indices; first_atom_is_ref = true)
-end
-
-#
-# If a list of atoms of PDBTools.Atom is provided
-#
-function contributions(
-    s::AtomSelection,
-    atom_contributions::Matrix{Float64},
-    atoms::Vector{PDBTools.Atom};
-    warning = true,
-)
-    (warning && s.nmols > 1) && warning_nmols_types()
-    indices = PDBTools.index.(atoms)
-    # Check which types of atoms belong to this selection
-    selected_types = which_types(s, indices, warning = warning)
-    return contributions(s, atom_contributions, selected_types)
-end
 
 #
 # If a residue of type PDBTools.Residue is provided
