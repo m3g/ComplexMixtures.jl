27de189ebbf5711060113749b1fabf9391b85615
Leandro Martinez <lmartine@unicamp.br>
2024-01-30 10:57:07 -0300

    rewritting contributions

--- src/tools/contributions.jl
+++ src/tools/contributions.jl
@@ -1,120 +1,95 @@
+"""
+    contributions(R::Result, group::Union{SoluteGroup,SolventGroup}; type = :mddf)
 
-function _error_contrib_index(i, itype)
-    throw(ArgumentError(chomp("""
-        Atom index $i with type index $itype are not available in the contribution matrix. 
-        There is something wrong in the way the group selection was provided.
-    """)))
-end
+Returns the contributions of the atoms of the solute or solvent to the MDDF, coordiantion number or MD count.
 
-function _error_contrib_name(name)
-    throw(ArgumentError(chomp("""
-        Atom name $name is not available in the contribution matrix. 
-        There is something wrong in the way the group selection was provided.
-    """)))
-end
-
-function coordination_number_to_mddf!(data, R::Result)
-    data ./= R.md_count_random
-    return data
-end
-
-
+"""
 function contributions(
     R::Result, 
     group::Union{SoluteGroup,SolventGroup};
     type = :mddf, 
 )
-    if typeof(group) == SoluteGroup
-        struct_data = R.solute
-        contrib_data = R.solute_group_contributions
-    elseif typeof(group) == SolventGroup
-        struct_data = R.solvent
-        contrib_data = R.solvent_group_contributions
+    if group isa SoluteGroup
+        sol = R.solute
+        group_count = R.solute_group_count
+    elseif group isa SolventGroup
+        sol = R.solvent
+        group_count = R.solvent_group_count
     end
 
-    c = zeros(size(contrib_data, 1))
+    sel_count = zeros(length(group_count[1]))
 
-    # If the index or group name was provided, just return the corresponding contribution
-    if isnothing(group.indices)
-        igroup = if !isnothing(group.group_index)  
-           group.group_index
-        elseif !isnothing(group.group_name) 
-           findfirst(==(group_name), struct_data.group_names)
+    # If the index of the groups was provided
+    if !isnothing(group.group_index)
+        igroup = group.group_index
+        if igroup > length(group_count)
+            throw(ArgumentError("Group $igroup not found in the group contribution count array."))
         end
-        if isnothing(igroup) || igroup > size(contrib_data, 2)
-            throw(ArgumentError("Group $igroup not found in the contribution matrix."))
-        end
-        c .= contrib_data[:, igroup]
+        sel_count .= group_count[igroup]
     end
+
+    # If the name of the group was provided
+    if !isnothing(group.group_name)
+        igroup = findfirst(==(group.group_name), sol.group_names)
+        if isnothing(igroup) || igroup > length(group_count)
+            throw(ArgumentError("Group $igroup not found in the group contribution count array."))
+        end
+        sel_count .= group_count[igroup]
+    end
+
     # If, instead, atom indices or names were provided, sum over the contributions of the atoms.
     # This sum is different if the structure contanis one or more than one molecule.
     # If the structure has more than one molecule, than the indices are indices of 
     # the atoms *within* the molecule. 
+
+    # Given atom inidices, sum over the contributions of the atoms
     if !isnothing(group.atom_indices) 
-        if struct_data.nmols == 1 
-            for i in group.atom_indices
-                i > size(contrib_data, 2) && _error_contrib_index(i, i)
-                c .+= @view(contrib_data[:, i])
-            end
-        else
-            for i in group.atom_indices
-                itype = (i - 1) ÷ struct_data.natomspermol + 1
-                itype > size(contrib_data, 2) && _error_contrib_index(i, itype)
-                c .+= @view(contrib_data[:, itype])
-            end
+        if isempty(group.atom_indices)
+            throw(ArgumentError("Group selection is empty"))
         end
-    end
-    if !isnothing(group.atom_names) 
-        if struct_data.nmols == 1 
-            for name in group.atom_names
-                i = findfirst(==(name), struct_data.names)
-                (isnothing(i) || i > size(contrib_data, 2)) && _error_contrib_name(name)
-                c .+= @view(contrib_data[:, i])
+        for i in group.atom_indices
+            itype = findfirst(==(i), sol.indices)
+            isnothing(itype) && throw(ArgumentError("Atom index $i not found in group data."))
+            if sol.nmols > 1
+                itype = atom_type(i, sol.natomspermol)
             end
-        else
-            for name in group.atom_names
-                i = findfirst(==(name), struct_data.names)
-                itype = (i - 1) ÷ struct_data.natomspermol + 1
-                (isnothing(i) || itype > size(contrib_data, 2)) && _error_contrib_name(name)
-                c .+= @view(contrib_data[:, itype])
-            end
+            sel_count .+= group_count[itype]
         end
     end
 
+    # Given atom names, sum over the contributions of the atoms
+    if !isnothing(group.atom_names) 
+        if isempty(group.atom_names)
+            throw(ArgumentError("Group selection is empty"))
+        end
+        for name in group.atom_names
+            itype = findfirst(==(name), sol.names)
+            isnothing(itype) && throw(ArgumentError("Atom name $name not found in group data."))
+            if sol.nmols > 1
+                itype = atom_type(i, sol.natomspermol)
+            end
+            sel_count .+= group_count[itype]
+        end
+    end
+
+    # Convert to the desired type
     if type == :mddf
-        for i in eachindex(R.md_count_random, c)
+        for i in eachindex(R.md_count_random, sel_count)
             if R.md_count_random[i] == 0.0
-                c[i] = 0.0
+                sel_count[i] = 0.0
             else
-                c[i] /= R.md_count_random[i]
+                sel_count[i] /= R.md_count_random[i]
             end
         end
     elseif type == :coordination_number
-        c .= cumsum(c)
+        sel_count .= cumsum(sel_count)
     elseif type == :md_count
         # do nothign, already md_count
     else
         throw(ArgumentError("type must be :mddf (default), :coordination_number or :md_count"))
     end
 
-    return c
-end
-
-
-#
-# If a residue of type PDBTools.Residue is provided
-#
-function contributions(
-    s::AtomSelection,
-    atom_contributions::Matrix{Float64},
-    residue::PDBTools.Residue;
-    warning = true,
-)
-    (warning && s.nmols > 1) && warning_nmols_types()
-    indices = collect(residue.range)
-    # Check which types of atoms belong to this selection
-    selected_types = which_types(s, indices, warning = warning)
-    return contributions(s, atom_contributions, selected_types)
+    return sel_count
 end
 
 function warning_nmols_types()
@@ -141,9 +116,9 @@ end
     traj = Trajectory("$dir/trajectory.pdb", solute, solvent, format = "PDBTraj")
     results = mddf(traj)
 
-    # solute contributions fetching
-    N_contributions = contributions(results, SoluteGroup("name N"))
-    @test length(N_contributions) == 500
+    # fetch contribution by atom name 
+    @test contributions(results, SoluteGroup("N")) == zeros(500)
+    @test contributions(results, SoluteGroup("N")) == zeros(500)
 
     C1_contributions = contributions(solute, SoluteGroup("name C1"))
     @test length(C1_contributions) == 500
