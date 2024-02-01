5668a5f0a4d19a696677ae2601c3c5aae6834c37
Leandro Martinez <lmartine@unicamp.br>
2024-02-01 08:16:44 -0300

    updated tests

--- test/namd_chemfiles.jl
+++ test/namd_chemfiles.jl
@@ -1,24 +1,23 @@
 
 @testitem "NAMD with ChemFiles" begin
-    using ComplexMixtures, PDBTools
-    using ComplexMixtures.Testing
-    const CM = ComplexMixtures
+    using ComplexMixtures: mddf, Trajectory, Options, AtomSelection, load
+    using PDBTools: readPDB, select
+    using ComplexMixtures.Testing: data_dir
 
     #
     # Tests with NAMD-DCD trajectory
     #
 
-    dir = "$(Testing.data_dir)/NAMD"
+    dir = "$data_dir/NAMD"
     atoms = readPDB("$dir/structure.pdb")
     options = Options(stride = 5, seed = 321, StableRNG = true, nthreads = 1, silent = true)
 
     # Example 1: protein-tmao
-
     # save(R,"$dir/protein_tmao.json")
-    R_save = load("$dir/protein_tmao.json"; legacy_warning = false)
+    R_save = load("$dir/protein_tmao.json")
     protein = AtomSelection(select(atoms, "protein"), nmols = 1)
     tmao = AtomSelection(select(atoms, "resname TMAO"), natomspermol = 14)
-    traj = CM.Trajectory("$dir/trajectory.dcd", protein, tmao, chemfiles = true)
+    traj = Trajectory("$dir/trajectory.dcd", protein, tmao, chemfiles = true)
     R = mddf(traj, options)
     @test isapprox(R, R_save, debug = true)
 
@@ -29,30 +28,27 @@
     @test R_load ≈ R_save
 
     # Example 2: water-tmao
-
     # save(R,"$dir/water_tmao.json")
-    R_save = load("$dir/water_tmao.json"; legacy_warning = false)
+    R_save = load("$dir/water_tmao.json")
     tmao = AtomSelection(select(atoms, "resname TMAO"), natomspermol = 14)
     water = AtomSelection(select(atoms, "water"), natomspermol = 3)
-    traj = CM.Trajectory("$dir/trajectory.dcd", tmao, water, chemfiles = true)
+    traj = Trajectory("$dir/trajectory.dcd", tmao, water, chemfiles = true)
     R = mddf(traj, options)
     @test isapprox(R, R_save, debug = true)
 
     # Example 3: tmao-tmao
-
     # save(R,"$dir/tmao_tmao.json")
-    R_save = load("$dir/tmao_tmao.json"; legacy_warning = false)
+    R_save = load("$dir/tmao_tmao.json")
     tmao = AtomSelection(select(atoms, "resname TMAO"), natomspermol = 14)
-    traj = CM.Trajectory("$dir/trajectory.dcd", tmao, tmao, chemfiles = true)
+    traj = Trajectory("$dir/trajectory.dcd", tmao, tmao, chemfiles = true)
     R = mddf(traj, options)
     @test isapprox(R, R_save, debug = true)
 
     # Example 3: water-water
-
     # save(R,"$dir/water_water.json")
     R_save = load("$dir/water_water.json"; legacy_warning = false)
     water = AtomSelection(select(atoms, "water"), natomspermol = 3)
-    traj = CM.Trajectory("$dir/trajectory.dcd", water, water, chemfiles = true)
+    traj = Trajectory("$dir/trajectory.dcd", water, water, chemfiles = true)
     R = mddf(traj, options)
     @test isapprox(R, R_save, debug = true)
 
