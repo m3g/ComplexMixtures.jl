f421f8cb3c9ec5f2dec77421b1fda2b2de293b3e
Leandro Martinez <lmartine@unicamp.br>
2024-02-01 14:00:37 -0300

    coordination number just calls contributions

--- src/tools/coordination_number.jl
+++ src/tools/coordination_number.jl
@@ -1,9 +1,10 @@
 """
     coordination_number(R::Result) = R.coordination_number
-    coordination_number(R::Result, group_contributions::Vector{Float64})
-    coordination_number(s::AtomSelection, atom_contributions::Matrix{Float64}, R::Result, group)
+    coordination_number(R::Result, s::Union{SoluteGroup,SolventGroup})
 
-Computes the coordination number of a given group of atoms from the solute or solvent atomic contributions to the MDDF. 
+Computes the coordination number of a given group of atoms of the solute or solvent
+
+atomic contributions to the MDDF. 
 If no group is defined (first call above), the coordination number of the whole solute or solvent is returned.
 
 If the `group_contributions` to the `mddf` are computed previously with the `contributions` function, the result can be used
@@ -58,36 +59,15 @@ residue50_coordination[findlast(<(5), R.d)]
 
 """
 function coordination_number end
-
 coordination_number(R::Result) = R.coordination_number
-
-function coordination_number(
-    s::AtomSelection,
-    atom_contributions::Matrix{Float64},
-    R::Result,
-    group,
-)
-    # Extract the group contributions to the MDDF
-    group_contributions = contributions(s, atom_contributions, group)
-    # Compute the coordination number
-    return coordination_number(R, group_contributions)
-end
-function coordination_number(R::Result, group_contributions::Vector{Float64})
-    # obs: we accumulate group coordination numbers, but at the end we 
-    # divide by random count of, so we the group contributions are the contributions 
-    # to the MDDF. To recover the coordination number we need to multiply back
-    # the group contributions by the random count.
-    cn = cumsum(
-        group_contributions[i] * R.md_count_random[i] for
-        i in eachindex(group_contributions)
-    )
-    return cn
-end
+coordination_number(R::Result, atsel::Union{SoluteGroup,SolventGroup}) = contributions(R, atsel; type = :coordination_number)
 
 @testitem "coordination_number" begin
-    using ComplexMixtures, PDBTools
-    using ComplexMixtures.Testing
-    dir = "$(Testing.data_dir)/NAMD"
+    using ComplexMixtures: coordination_number, contributions, mddf, Trajectory, Options, AtomSelection, load
+    using PDBTools: readPDB, select
+    using ComplexMixtures.Testing: data_dir
+
+    dir = "$data_dir/NAMD"
     atoms = readPDB("$dir/structure.pdb")
     protein = AtomSelection(select(atoms, "protein"), nmols = 1)
     tmao = AtomSelection(select(atoms, "resname TMAO"), natomspermol = 14)
@@ -96,8 +76,9 @@ end
     R = mddf(traj, options)
 
     # Test self-consistency
-    @test sum(R.solute_atom) ≈ sum(R.solvent_atom)
-    @test coordination_number(R, contributions(protein, R.solute_atom, select(atoms, "protein"))) == R.coordination_number
+    @test sum(sum.(R.solute_group_count)) ≈ sum(sum.(R.solvent_group_count))
+    @test coordination_number(R) == R.coordination_number
+    @test coordination_number(R, SoluteGroup(R.solute)) == R.coordination_number
 
     # Checked with vmd: same residue as (resname TMAO and within 3.0 of protein)
     @test R.coordination_number[findfirst(>(3), R.d)] == 7.0
@@ -105,13 +86,8 @@ end
 
     # THR93 forms a hydrogen bond with TMAO in this frame
     thr93 = select(atoms, "protein and residue 93")
-    @test last(coordination_number(R, contributions(protein, R.solute_atom, thr93))) == 1
+    @test last(coordination_number(R, SoluteGroup(thr93))) == 1
 
-    # Consistency of a read-out result
-    pdb = readPDB("$(Testing.data_dir)/NAMD/Protein_in_Glycerol/system.pdb")
-    R = load("$(Testing.data_dir)/NAMD/Protein_in_Glycerol/protein_water.json"; legacy_warning=false)
-    group = PDBTools.select(pdb, "protein")
-    solute = AtomSelection(group, nmols = 1)
-    cn = coordination_number(solute, R.solute_atom, R, group)
-    @test maximum(R.coordination_number .- cn) < 1e-10
+    # Test coordination number of a solvent atom
+    @test sum(coordination_number(R, SolventGroup(["O1"]))) == 1171.0
 end
