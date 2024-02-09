3f6b9e3ae6054b6406bdf704c490e9822a422757
Leandro Martinez <lmartine@unicamp.br>
2024-02-09 10:12:48 -0300

    run doc tests in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,12 @@
-using TestItemRunner
+using TestItemRunner: @run_package_tests, @testitem
 @run_package_tests 
 
 @testitem "Aqua.test_all" begin
     import Aqua
     Aqua.test_all(ComplexMixtures)
+end
+
+@testitem "Doctests" begin
+    using Documenter: doctest
+    doctest(ComplexMixtures)
 end
