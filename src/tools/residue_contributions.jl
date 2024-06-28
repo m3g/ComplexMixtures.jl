77afd575076d3b7978875475bc1cb44d1f79b082
Leandro Martinez <lmartine@unicamp.br>
2024-06-28 14:14:13 -0300

    add docs and other small fixes

--- src/tools/residue_contributions.jl
+++ src/tools/residue_contributions.jl
@@ -1,9 +1,97 @@
+"""
+    ResidueContributions
+
+Structure to store the residue contributions to the solute-solvent pair distribution function.
+
+A structure of type `ResultContributions` can be used to plot the residue contributions to the solute-solvent pair distribution function,
+or to perform arithmetic operations with other `ResidueContributions` objects.
+
+# Examples
+
+```julia
+using ComplexMixturs, PDBTools, Plots
+...
+result = mddf(traj, options)
+rc = ResidueContributions(result, select(atoms, "protein"))
+contourf(rc) # plots a contour map
+```
+
+```julia
+using ComplexMixturs, PDBTools, Plots
+...
+result = mddf(traj2, options)
+rc1 = ResidueContributions(result, select(atoms, "protein"))
+result = mddf(traj2, options)
+rc2 = ResidueContributions(result, select(atoms, "protein"))
+rc_diff = rc2 - rc1
+contourf(rc_diff) # plots a contour map of the difference
+```
+
+"""
 struct ResidueContributions
     d::Vector{Float64}
     xticks::Tuple{StepRange{Int,Int},Vector{String}}
     residue_contributions::Matrix{Float64}
 end
 
+"""
+    ResidueContributions(
+        results::Result, atoms::AbstractVector{PDBTools.Atom};
+        residue_range=nothing,
+        dmin=1.5, dmax=3.5,
+        type=:mddf,
+    )
+
+Compute the residue contributions to the solute-solvent pair distribution function.
+The function returns a `ResidueContributions` object that can be used to plot the residue contributions,
+or to perform arithmetic operations with other `ResidueContributions` objects.
+
+# Arguments
+
+- `results::Result`: The result object obtained from the `mddf` function.
+- `atoms::AbstractVector{PDBTools.Atom}`: The vector of atoms of the solute, or a part of it.
+
+# Optional arguments
+
+- `residue_range::UnitRange{Int}`: The range of residues to consider. By default, it considers all residues.
+- `dmin::Float64`: The minimum distance to consider. Default is `1.5`.
+- `dmax::Float64`: The maximum distance to consider. Default is `3.5`.
+- `type::Symbol`: The type of the pair distribution function (`:mddf`, `:md_count`, or `:coordination_number`). Default is `:mddf`.
+
+# Example
+
+```julia-repl
+julia> using ComplexMixtures, PDBTools
+
+julia> using ComplexMixtures.Testing
+
+julia> atoms = readPDB(pdbfile);
+
+julia> protein = AtomSelection(select(atoms, "protein"), nmols=1);
+
+julia> water = AtomSelection(select(atoms, "water"), natomspermol=3);
+
+julia> traj = Trajectory("\$data_dir/NAMD/trajectory.dcd", protein, water);
+
+julia> results = mddf(traj, Options(bulk_range=(8.0, 12.0)))
+
+julia> rc = ResidueContributions(result, select(atoms, "protein"))
+         Residue Contributions
+    3.51 ████████████████████████████████████████████████████
+    3.27 ███████████   ████████ ████████████████ ████████████
+    3.03 █████ █████   ████████    █████████████ ████████████
+    2.79 █████ █████   ████████    █████████████ ████████████
+ d  2.55 █████ █████   ████████    █████████████ ████████████
+    2.31 █████ █████   ████████    █████████████ ████████████
+    2.07 █████ █████   ████████    █████████████ ████████████
+    1.83 ███████████████████████████████████████ ████████████
+    1.59 ████████████████████████████████████████████████████
+         A1      S17     V33     D49     G65     N81     G97     
+```
+
+The `rc` object of the example can be plotted using the `contourf` function of `Plots`.
+
+"""
 function ResidueContributions(
     results::Result, atoms::AbstractVector{PDBTools.Atom};
     residue_range=nothing,
@@ -14,7 +102,10 @@ function ResidueContributions(
     if length(atoms) == 0
         throw(ArgumentError("""\n
             The atom selection provided is empty.
-        """)) 
+        """))
+    end
+    if results.solute.custom_groups
+        _custom_group_error_for_ResidueContributions()
     end
 
     # collect the list of residues (using PDBTools)
@@ -26,9 +117,9 @@ function ResidueContributions(
     ilast = findfirst(res -> PDBTools.resnum(res) == last(residue_range), residues)
 
     # Check if the range is fine
-    if isnothing(ifirst) || isnothing(ilast) 
+    if isnothing(ifirst) || isnothing(ilast)
         throw(ArgumentError("""\n
-        
+
             The residue_range $residue_range is out of bounds.
 
             The first and last residue numbers of the selection are: $(PDBTools.resnum(first(residues))) and $(PDBTools.resnum(last(residues))).
@@ -52,6 +143,8 @@ function ResidueContributions(
     # find the indexes of the distances that are within the range
     idmin = findfirst(d -> d > dmin, results.d)
     idmax = findfirst(d -> d > dmax, results.d)
+    isnothing(idmin) && (idmin = 1)
+    isnothing(idmax) && (idmax = length(results.d))
 
     # Obtain pretty labels for the residues in the x-axis (using PDBTools)
     xticks = PDBTools.residue_ticks(atoms;
@@ -70,10 +163,10 @@ end
 
 function _check_identity_of_residues(rc1::ResidueContributions, rc2::ResidueContributions)
     if rc1.xticks != rc2.xticks
-        throw(ArgumentError("The residues in the two ResidueContributions objects are different"))
+        throw(ArgumentError("The residues in the two ResidueContributions objects differ."))
     end
     if rc1.d != rc2.d
-        throw(ArgumentError("The distances in the two ResidueContributions objects are different"))
+        throw(ArgumentError("The distances in the two ResidueContributions objects differ.")) 
     end
     return nothing
 end
@@ -109,7 +202,7 @@ end
 
 function *(rc1::ResidueContributions, rc2::ResidueContributions)
     _check_identity_of_residues(rc1, rc2)
-    return ResidueContributions(rc1.d, rc.xticks, rc1.residue_contributions .* rc2.residue_contributions)
+    return ResidueContributions(rc1.d, rc1.xticks, rc1.residue_contributions .* rc2.residue_contributions)
 end
 
 const tempo_color_scheme = [
@@ -172,6 +265,81 @@ function Base.show(io::IO, ::MIME"text/plain", rc::ResidueContributions)
     print(io, map)
 end
 
-@testitem "residue contributions" begin
+function _custom_group_error_for_ResidueContributions()
+    throw(ArgumentError("""\n
+
+        ResidueContribution cannot be run if the MDDFs were computed with custom groups.
+
+        This is because it requires the contribution of each independent atom to accumulate
+        the residue contributions. The second argument of the function must be the vector of 
+        atoms of the solute, not a group selection.
+
+        Please read the documention, by typing: `? ResidueContribution`
+
+    """))
+end
+
+# Custom error message for common mistaken call
+ResidueContributions(result, g::Union{SoluteGroup,SolventGroup}, args...; kwargs...) = 
+    _custom_group_error_for_ResidueContributions()
+
+@testitem "ResidueContribution" begin
+    using ComplexMixtures
+    using PDBTools
+    using ComplexMixtures.Testing: data_dir, pdbfile
+
+    atoms = readPDB(pdbfile)
+    protein = AtomSelection(select(atoms, "protein"), nmols=1)
+    water = AtomSelection(select(atoms, "water"), natomspermol=3)
+    traj = Trajectory("$data_dir/NAMD/trajectory.dcd", protein, water)
+    options = Options(;
+        seed=1,
+        stride=1,
+        StableRNG=true,
+        nthreads=2,
+        silent=true
+    )
+    result = mddf(traj, options)
+
+    # essential properties
+    rc = ResidueContributions(result, select(atoms, "protein"))
+    @test length(rc.d) == 101 
+    @test length.(rc.xticks) == (104, 104)
+    @test size(rc.residue_contributions) == (101, 104)
+    rc = ResidueContributions(result, select(atoms, "protein"); dmin=0.0, dmax=10.0)
+    @test contributions(result, SoluteGroup(select(atoms, "protein and resnum 1"))) ≈ rc.residue_contributions[:, 1] 
+    @test first(rc.d) == first(result.d)
+    @test last(rc.d) == last(result.d)
+
+    # arithmetic operations
+    rc = ResidueContributions(result, select(atoms, "protein"))
+    rc2 = ResidueContributions(result, select(atoms, "protein"))
+    @test all(rc.residue_contributions .- rc2.residue_contributions .< 1e-10)
+    @test all(rc.residue_contributions + rc2.residue_contributions .≈ 2 .* rc.residue_contributions)
+    rdiv = rc / rc2
+    @test all(x -> isapprox(x, 1.0), filter(>(0.5), rdiv.residue_contributions))
+    @test all(<(1.e-10), filter(<(0.5), rdiv.residue_contributions))
+    rmul = rc * rc2
+    @test rmul.residue_contributions ≈ rc.residue_contributions .^2
+
+    # Error messages
+    rc2 = ResidueContributions(result, select(atoms, "protein and residue < 50"))
+    @test_throws ArgumentError rc - rc2
+    rc2 = ResidueContributions(result, select(atoms, "protein"); dmax = 3.0)
+    @test_throws ArgumentError rc - rc2
+    @test_throws ArgumentError ResidueContributions(result, select(atoms, "protein and resname XXX"))
+    @test_throws ArgumentError ResidueContributions(result, select(atoms, "protein"); residue_range=1:300)
+    @test_throws ArgumentError ResidueContributions(result, SoluteGroup("ALA"))
+
+    acidic_residues = select(atoms, "protein and acidic");
+    basic_residues = select(atoms, "protein and basic");
+    protein = AtomSelection(select(atoms, "protein"), nmols=1,
+        group_atom_indices = [ index.(acidic_residues), index.(basic_residues) ],
+        group_names = [ "acidic residues", "basic residues" ]
+    )
+    traj = Trajectory("$data_dir/NAMD/trajectory.dcd", protein, water)
+    result = mddf(traj, Options(lastframe=2))
+    @test_throws ArgumentError ResidueContributions(result, select(atoms, "protein"))
+    
 
 end
