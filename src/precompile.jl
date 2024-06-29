ea322b8a972a378187945d700a1d91a1fe747e4f
Leandro Martinez <lmartine@unicamp.br>
2024-06-28 22:17:09 -0300

    update precompilation directives

--- src/precompile.jl
+++ src/precompile.jl
@@ -2,17 +2,17 @@ PrecompileTools.@setup_workload begin
     # Putting some things in `@setup_workload` instead of `@compile_workload` can reduce the size of the
     # precompile file and potentially make loading faster.
     dir = "$(Testing.data_dir)/NAMD"
-    atoms = PDBTools.readPDB("$dir/structure.pdb", "resname TMAO")
-    tmao1 = AtomSelection(PDBTools.select(atoms, "resname TMAO and resnum 1"), natomspermol = 14)
+    atoms = PDBTools.readPDB("$dir/structure.pdb", "protein or resname TMAO")
+    prot = PDBTools.select(atoms, "protein and resnum < 4")
+    tmao = PDBTools.select(atoms, "resname TMAO and resnum <= 2")
     PrecompileTools.@compile_workload begin
-        options = Options(lastframe = -1, silent = true)
-        tmao2 = AtomSelection(
-            PDBTools.select(atoms, "resname TMAO and resnum 2 or resname TMAO and resnum 3"),
-            natomspermol = 14,
-        )
-        traj = Trajectory("$dir/trajectory.dcd", tmao1, tmao2)
+        options = Options(lastframe = 1, silent = true, n_random_samples=1)
+        solute = AtomSelection(prot, nmols = 1)
+        solvent = AtomSelection(tmao, natomspermol = 14)
+        traj = Trajectory("$dir/trajectory.dcd", solute, solvent)
         R = mddf(traj, options)
-        traj = Trajectory("$dir/trajectory.dcd", tmao2)
+        rc = ResidueContributions(R, prot)
+        traj = Trajectory("$dir/trajectory.dcd", solvent)
         R = mddf(traj, options)
     end
 end
