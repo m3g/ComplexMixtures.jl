b54d69c5b8f2739b80a3c093afd8f71d140f38e0
Leandro Martinez <lmartine@unicamp.br>
2024-06-30 22:27:13 -0300

    support all kw parameters, and better automatic color choice

--- ext/Plotting.jl
+++ ext/Plotting.jl
@@ -4,7 +4,8 @@ using TestItems: @testitem
 import Plots
 import ComplexMixtures
 import PDBTools
-using ComplexMixtures: Result, SoluteGroup, SolventGroup, contributions, ResidueContributions
+using ComplexMixtures: Result, SoluteGroup, SolventGroup, contributions, 
+                       ResidueContributions, _set_clims_and_colorscale!
 using PDBTools: Residue, residue_ticks, Atom, eachresidue, resnum
 
 """
@@ -14,8 +15,6 @@ using PDBTools: Residue, residue_ticks, Atom, eachresidue, resnum
         oneletter=false,
         xlabel="Residue",
         ylabel="r / Å",
-        clims=nothing,
-        colorscale=:tempo,
     )
 
 Plot the contribution of each residue to the solute-solvent pair distribution function as a contour plot.
@@ -31,9 +30,10 @@ This function requires loading the `Plots` package.
 - `step`: The step of the residue ticks in the x-axis of the plot. Default is 1.
 - `oneletter::Bool`: Use one-letter residue codes. Default is `false`. One-letter codes are only available for the 20 standard amino acids.
 - `xlabel` and `ylabel`: Labels for the x and y axes. Default is `"Residue"` and `"r / Å"`.
-- `clims`: The color limits for the contour plot.
-- `colorscale`: The color scale for the contour plot. Default is `:tempo`. We suggest `:bwr` if the zero
-  is at the middle of the color scale (use `clims` to adjust the color limits).  
+
+This function will set color limits and choose the scale automatically. These parameters can be set
+with the `clims` and `color` arguments of `Plots.contourf`. Other plot customizations can be 
+done by passing other keyword arguments to this function, which will be passed to `Plots.contourf`.
 
 # Example
 
@@ -52,15 +52,17 @@ julia> plt = contourf(rc; step=5)
 This will produce a plot with the contribution of each residue to the solute-solvent pair distribution function,
 as a contour plot, with the residues in the x-axis and the distance in the y-axis.
 
-The resulting plot can customized using the standard mutating `plot!` function, for example, 
+To customize the plot, use the `Plot.contourf` keyword parameters, for example:
 
 ```julia-repl
-julia> plot!(plt, size=(800, 400), title="Contribution per residue")
+julia> plt = contourf(rc; step=5, size=(800,400), title="Title", clims=(-0.1, 0.1))
 ```
 
 !!! compat
     This function requires loading the `Plots` package and is available in
     ComplexMixtures v2.5.0 or greater.
+
+    Support for all `Plots.contourf` parameters was introduced in ComplexMixtures v2.6.0.
 
 """
 function Plots.contourf(
@@ -70,7 +72,8 @@ function Plots.contourf(
     xlabel="Residue",
     ylabel="r / Å",
     clims=nothing,
-    colorscale=nothing,
+    color=nothing,
+    kargs...
 )
 
     residue_numbers = [ parse(Int, label[4:end]) for label in rc.xticks[2] ]
@@ -88,27 +91,17 @@ function Plots.contourf(
     else
         (tick_marks, tick_labels) 
     end
-
-    if isnothing(clims)
-        minval, maxval = extrema(rc.residue_contributions)
-        if minval >= 0
-            clims = (0, maxval)
-            isnothing(colorscale) && (colorscale = :tempo)
-            levels = 5
-        elseif maxval <= 0
-            clims = (minval, 0)
-            isnothing(colorscale) && (colorscale = :tempo)
-            levels = 5
-        else
-            mval = max(abs(minval), maxval)
-            clims = (-mval, mval)
-            isnothing(colorscale) && (colorscale = :bwr)
-            levels = 11
-        end
+    clims, colorscale = _set_clims_and_colorscale!(rc; clims, colorscale=color)
+    if colorscale == :tempo
+        levels = 5
+    else
+        levels = 12
     end
 
     Plots.default(fontfamily="Computer Modern")
-    plt = Plots.contourf(residue_numbers, rc.d, rc.residue_contributions,
+    plt = Plots.contourf(
+        residue_numbers, 
+        rc.d, rc.residue_contributions;
         color=Plots.cgrad(colorscale),
         linewidth=1,
         linecolor=:black,
@@ -121,6 +114,7 @@ function Plots.contourf(
         margin=0.5Plots.PlotMeasures.cm,
         framestyle=:box,
         clims=clims,
+        kargs...
     )
 
     # return the plot
@@ -224,15 +218,30 @@ end
     # Load example output file (computed in the previous script)
     protein = readPDB(joinpath(Testing.data_dir, "Gromacs/system.pdb"), "protein")
     results = load(joinpath(Testing.data_dir, "Gromacs/protein_EMI.json"))
+    tmpplot = tempname() * ".png"
 
     rc = ResidueContributions(results, select(protein, "residue >= 50 and residue <= 75"))
     plt = contourf(rc)
-    tmpplot = tempname() * ".png"
+    savefig(plt, tmpplot)
+    @test isfile(tmpplot)
+
+    # different color scale
+    rc2 = copy(rc)
+    rc2.residue_contributions[:,10:end] .*= -1
+    plt = contourf(rc2)
+    savefig(plt, tmpplot)
+    @test isfile(tmpplot)
+    rc3 = copy(rc)
+    rc3.residue_contributions .*= -1
+    plt = contourf(rc3)
+    savefig(plt, tmpplot)
+    @test isfile(tmpplot)
+
+    plt = contourf(rc)
     savefig(plt, tmpplot)
     @test isfile(tmpplot)
 
     plt = contourf_per_residue(results, protein; residue_range=50:75, oneletter=true)
-    tmpplot = tempname() * ".png"
     savefig(plt, tmpplot)
     @test isfile(tmpplot)
 
