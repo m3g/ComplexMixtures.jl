5fb1101aa147bd76055490cad9e187b725c7df03
lmiq <lmartine@unicamp.br>
2025-11-02 21:26:39 -0300

    move testing data to main module

--- src/ComplexMixtures.jl
+++ src/ComplexMixtures.jl
@@ -37,7 +37,10 @@ export gr
 export grid3D
 
 # Testing data
-include("./testing.jl")
+const src_dir = @__DIR__
+const test_dir = joinpath(src_dir,"../test")
+const data_dir = normpath("$test_dir/data")
+const pdb_file_example = normpath("$data_dir/NAMD/structure.pdb")
 
 # Input and Output data structures
 include("./io.jl")
--- src/Testing.jl
+++
@@ -1,4 +0,0 @@
-const src_dir = @__DIR__
-const test_dir = joinpath(src_dir,"../test")
-const data_dir = normpath("$test_dir/data")
-const pdb_file_example = normpath("$data_dir/NAMD/structure.pdb")
