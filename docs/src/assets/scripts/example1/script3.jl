0ccf085cdfc3ccbf955ca7717031e519d97b77a2
Leandro Martinez <lmartine@unicamp.br>
2025-12-15 11:53:33 -0300

    remove unused code from example script3

--- docs/src/assets/scripts/example1/script3.jl
+++ docs/src/assets/scripts/example1/script3.jl
@@ -9,10 +9,6 @@ atoms = read_pdb("./system.pdb")
 # Load results of a ComplexMixtures run
 results = load("./glyc50_results.json")
 
-# Inform which is the solute
-protein = select(atoms, "protein")
-solute = AtomSelection(protein, nmols=1)
-
 # Compute the 3D density grid and output it to the PDB file
 # here we use dmax=3.5 such that the the output file is not too large
 grid = grid3D(results, atoms, "./grid.pdb"; dmin=1.5, dmax=3.5)
